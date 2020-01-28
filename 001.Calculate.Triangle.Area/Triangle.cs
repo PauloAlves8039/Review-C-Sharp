ebe9cb4390e8ef11ebea392db2c656eb6aaa8a32
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-28 12:07:38 -0300

    Adicionando a classe Triangle

---
+++ 001.Calculate.Triangle.Area/Triangle.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Arquivo: classe Triangle
+ * Autor: Paulo Alves
+ * Descrição: responsável por conter propriedades e método para calcular área de triângulo
+ * Data: 28/01/2020
+*/
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _001.Calculate.Triangle.Area
+{
+    class Triangle
+    {
+        public int A;
+        public int B;
+        public int C;
+
+        public double Area()
+        {
+            double p = (A + B + C) / 2.0;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+    }
+}
