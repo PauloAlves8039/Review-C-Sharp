b578301e2d7a0ad52b50692872ee5cc78946241e
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-28 12:19:53 -0300

    Adicionando lógica para saída de dados

--- 001.Calculate.Triangle.Area/Program.cs
+++ 001.Calculate.Triangle.Area/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _001.Calculate.Triangle.Area
 {
@@ -6,7 +7,33 @@ namespace _001.Calculate.Triangle.Area
     {
         static void Main(string[] args)
         {
-            
+            Triangle x = new Triangle();
+            Triangle y = new Triangle();
+
+            Console.WriteLine("Enter the measures of triangle X: ");
+            x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            x.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine("Enter the measures of triangle Y: ");
+            y.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double areaX = x.Area();
+            double areaY = y.Area();
+
+            Console.WriteLine("The area of X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
+            Console.WriteLine("The area of Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
+
+            if (areaX > areaY)
+            {
+                Console.WriteLine("Larger area: X");
+            }
+            else
+            {
+                Console.WriteLine("Larger area: Y");
+            }
         }
     }
 }
