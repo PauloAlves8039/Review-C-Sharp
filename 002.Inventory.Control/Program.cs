76d95d8ffa61a3c76753dab4518757bdbae082ac
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-29 11:41:16 -0300

    Saída de dados para o controle de estoque

--- 002.Inventory.Control/Program.cs
+++ 002.Inventory.Control/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _002.Inventory.Control
 {
@@ -6,7 +7,34 @@ namespace _002.Inventory.Control
     {
         static void Main(string[] args)
         {
-            
+            Product p = new Product();
+
+            Console.WriteLine("Enter product data: ");
+            Console.Write("Name: ");
+            p.Name = Console.ReadLine();
+            Console.Write("Price: ");
+            p.Price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Quantity in stock: ");
+            p.Quantity = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine("Product Data: " + p);
+
+            Console.WriteLine();
+            Console.Write("Enter the number of products to be added to the stock: ");
+            int qty = int.Parse(Console.ReadLine());
+            p.AddProducts(qty);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated data: " + p);
+
+            Console.WriteLine();
+            Console.Write("Enter the number of products to be removed from stock: ");
+            qty = int.Parse(Console.ReadLine());
+            p.RemoveProducts(qty);
+
+            Console.WriteLine();
+            Console.WriteLine("Updated data: " + p);
         }
     }
 }
