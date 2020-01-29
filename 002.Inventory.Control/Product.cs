dead02fa48e679de82ca9bfce87cde3ec934245d
PauloAlves8039 <pj.alves547@gmail.com>
2020-01-29 11:04:07 -0300

    Adicionando a classe Product

---
+++ 002.Inventory.Control/Product.cs
@@ -0,0 +1,44 @@
+﻿/*
+ * Arquivo: classe Product
+ * Autor: Paulo Alves
+ * Descrição: responsável por conter propriedades e métodos para controle de produtos em um estoque
+ * Data: 29/01/2020
+*/
+
+using System.Globalization;
+
+namespace _002.Inventory.Control
+{
+    class Product
+    {
+        public string Name;
+        public double Price;
+        public int Quantity;
+
+        public double TotalValueStock()
+        {
+            return Price * Quantity;
+        }
+
+        public void AddProducts(int quantity)
+        {
+            Quantity += quantity;
+        }
+
+        public void RemoveProducts(int quantity)
+        {
+            Quantity -= quantity;
+        }
+
+        public override string ToString()
+        {
+            return Name
+                + ", $ "
+                + Price.ToString("F2", CultureInfo.InvariantCulture)
+                + ", "
+                + Quantity
+                + " units, Total: $ "
+                + TotalValueStock().ToString("F2", CultureInfo.InvariantCulture);
+        }
+    }
+}
