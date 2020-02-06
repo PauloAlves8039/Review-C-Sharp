7f5985eaa06def39ce651cca7306746f758e6bd2
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-06 11:36:09 -0300

    Documentando estrutura da classe Product

--- 002.Inventory.Control/Product.cs
+++ 002.Inventory.Control/Product.cs
@@ -1,35 +1,53 @@
-﻿/*
- * Arquivo: classe Product
- * Autor: Paulo Alves
- * Descrição: responsável por conter propriedades e métodos para controle de produtos em um estoque
- * Data: 29/01/2020
-*/
-
-using System.Globalization;
+﻿using System.Globalization;
 
 namespace _002.Inventory.Control
 {
+    /// <summary>
+    /// Classe responsável por conter propriedades e métodos para controle de produtos em um estoque.
+    /// </summary>
     class Product
     {
+        /// <value>Representa o nome do produto.</value>
         public string Name;
+
+        /// <value>Atribui um preço ao produto.</value>
         public double Price;
+
+        /// <value>Informa a quantidade do produto.</value>
         public int Quantity;
 
+
+        /// <summary>
+        /// Método para calcular o valor total dos produtos no estoque.
+        /// </summary>
+        /// <returns>Retorna a multiplicação do preço X quantidade dos produtos.</returns>
         public double TotalValueStock()
         {
             return Price * Quantity;
         }
 
+        /// <summary>
+        /// Método para adicionar produto no estoque.
+        /// </summary>
+        /// <param name="quantity">Parâmetro inteiro para adição dos produtos.</param>
         public void AddProducts(int quantity)
         {
             Quantity += quantity;
         }
 
+        /// <summary>
+        /// Método para remover produto no estoque.
+        /// </summary>
+        /// <param name="quantity">Parâmetro inteiro para remoção dos produtos.</param>
         public void RemoveProducts(int quantity)
         {
             Quantity -= quantity;
         }
 
+        /// <summary>
+        /// Sobrescrita do método ToString para saída de dados do Produtos.
+        /// </summary>
+        /// <returns>Retorna o resultado atual dos produtos no estoque através de suas propriedades e o método TotalValueStock().</returns>
         public override string ToString()
         {
             return Name
