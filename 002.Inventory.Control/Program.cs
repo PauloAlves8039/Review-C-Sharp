d39dbac87c7d7c39da73370301397285bb6a9889
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-06 11:53:05 -0300

    Documentando estrutura da classe Program

--- 002.Inventory.Control/Program.cs
+++ 002.Inventory.Control/Program.cs
@@ -3,12 +3,22 @@ using System.Globalization;
 
 namespace _002.Inventory.Control
 {
+    /// <summary>
+    /// Classe que representa ponto de entrada e saída de dados dos produtos
+    /// </summary>
+    /// <remarks>Classe utiliza método Main para execução da aplicação.</remarks>
     class Program
     {
         static void Main(string[] args)
         {
+            /// <summary>
+            /// Instância da classe Product para representar a entidade produto.
+            /// </summary>
             Product p = new Product();
 
+            /// <summary>
+            /// Entrada de dados.
+            /// </summary>
             Console.WriteLine("Enter product data: ");
             Console.Write("Name: ");
             p.Name = Console.ReadLine();
@@ -17,9 +27,16 @@ namespace _002.Inventory.Control
             Console.Write("Quantity in stock: ");
             p.Quantity = int.Parse(Console.ReadLine());
 
+            /// <summary>
+            /// Informa a saída dos dados atualizados do estoque.
+            /// </summary>
             Console.WriteLine();
             Console.WriteLine("Product Data: " + p);
 
+            /// <summary>
+            /// Entrada de dados com chamada do método AddProducts()
+            /// </summary>
+            /// <param name="qty">Parâmetro para representar a propriedade Quantity.</param>
             Console.WriteLine();
             Console.Write("Enter the number of products to be added to the stock: ");
             int qty = int.Parse(Console.ReadLine());
@@ -28,6 +45,9 @@ namespace _002.Inventory.Control
             Console.WriteLine();
             Console.WriteLine("Updated data: " + p);
 
+            /// <summary>
+            /// Entrada de dados com chamada do método RemoveProducts()
+            /// </summary>
             Console.WriteLine();
             Console.Write("Enter the number of products to be removed from stock: ");
             qty = int.Parse(Console.ReadLine());
