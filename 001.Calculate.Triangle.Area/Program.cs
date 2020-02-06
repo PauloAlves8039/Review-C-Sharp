3bd00f324f64198e5d8c5611733714bfe8c16f07
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-06 10:54:11 -0300

    Documentando estrutura da classe Program

--- 001.Calculate.Triangle.Area/Program.cs
+++ 001.Calculate.Triangle.Area/Program.cs
@@ -3,13 +3,23 @@ using System.Globalization;
 
 namespace _001.Calculate.Triangle.Area
 {
+    /// <summary>
+    /// Classe que representa ponto de entrada e saída de dados da aplicação.
+    /// </summary>
+    /// <remarks>Classe utiliza método Main para execução da aplicação.</remarks>
     class Program
     {
         static void Main(string[] args)
         {
+            /// <summary>
+            /// Instâncias da classe Triangle para representar os triângulos X e Y.
+            /// </summary>
             Triangle x = new Triangle();
             Triangle y = new Triangle();
 
+            /// <summary>
+            /// Entrada de dados
+            /// </summary>
             Console.WriteLine("Enter the measures of triangle X: ");
             x.A = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             x.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
@@ -20,12 +30,21 @@ namespace _001.Calculate.Triangle.Area
             y.B = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
             y.C = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
 
+            /// <value>Obtêm medida do triângulo X.</value>
             double areaX = x.Area();
+
+            /// <value>Obtêm medida do triângulo Y.</value>
             double areaY = y.Area();
 
+            /// <summary>
+            /// Saída de dados
+            /// </summary>
             Console.WriteLine("The area of X = " + areaX.ToString("F4", CultureInfo.InvariantCulture));
             Console.WriteLine("The area of Y = " + areaY.ToString("F4", CultureInfo.InvariantCulture));
 
+            /// <summary>
+            /// Verificando qual dos triângulos possui maior área
+            /// </summary>
             if (areaX > areaY)
             {
                 Console.WriteLine("Larger area: X");
