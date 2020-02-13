dd170245f51a3890477868a3a52300fab535973c
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-13 13:05:14 -0300

    Adicionando classe com membros estáticos

---
+++ 003.Calculate.Radius/Calculator.cs
@@ -0,0 +1,29 @@
+﻿namespace _003.Calculate.Radius
+{
+    /// <summary>
+    /// Classe responsável por calcular valor do raio utilizando membros estáticos
+    /// </summary>
+    class Calculator
+    {
+        /// <value>Representa o valor PI.</value>
+        public static double Pi = 3.14;
+
+        /// <summary>
+        /// Método para calcular valor da circunferência
+        /// </summary>
+        /// <param name="r">Recebe o valor do raio</param>
+        /// <returns>Retorna o valor 2.0 multiplicado pelo valor de Pi e pelo valor do raio</returns>
+        public static double Circumference(double r) {
+            return 2.0 * Pi * r;
+        }
+
+        /// <summary>
+        /// Métoddo para calcular volume
+        /// </summary>
+        /// <param name="r">Recebe o valor do raio</param>
+        /// <returns>Retorna quatro terços de Pi multiplicando o valor do raio ao cubo</returns>
+        public static double Bulk(double r) {
+            return 4.0 / 3.0 * Pi * r * r * r;
+        }
+    }
+}
