dc9a50662797f589e8c4d17bfbb33e223ee9f72d
PauloAlves8039 <pj.alves547@gmail.com>
2020-02-13 13:20:18 -0300

    Saída de dados para valor do raio

--- 003.Calculate.Radius/Program.cs
+++ 003.Calculate.Radius/Program.cs
@@ -1,12 +1,25 @@
 ﻿using System;
+using System.Globalization;
 
 namespace _003.Calculate.Radius
 {
+    /// <summary>
+    /// Classe que representa ponto de entrada e saída de dados referentes ao valor do raio
+    /// <remarks>Classe utiliza método Main para execução da aplicação.</remarks>
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
-            
+            Console.Write("Enter the radius value: ");
+            double radius = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            double circ = Calculator.Circumference(radius);
+            double bulk = Calculator.Bulk(radius);
+
+            Console.WriteLine("Circumference: " + circ.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Bulk: " + bulk.ToString("F2", CultureInfo.InvariantCulture));
+            Console.WriteLine("Value of PI: " + Calculator.Pi.ToString("F2", CultureInfo.InvariantCulture));
         }
     }
 }
